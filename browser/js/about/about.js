811ec1c2bef9f1f2aa3d31592ea46b4573d6f2f3
Scott <scotteggs@gmail.com>
2015-11-19 18:23:41 -0500

    got a map to appear

--- browser/js/about/about.js
+++ browser/js/about/about.js
@@ -9,9 +9,26 @@ app.config(function ($stateProvider) {
 
 });
 
-app.controller('AboutController', function ($scope, FullstackPics) {
-
-    // Images of beautiful Fullstack people.
-    $scope.images = _.shuffle(FullstackPics);
-
+app.controller('AboutController', function ($scope) {
+    // make map
+    angular.extend($scope, {
+        san_fran: {
+            lat: 37.78,
+            lng: -122.42,
+            zoom: 13
+        },
+        events: {},
+        layers: {
+            baselayers: {
+                osm: {
+                    name: 'OpenStreetMap',
+                    url: 'https://{s}.tiles.mapbox.com/v3/examples.map-i875mjb7/{z}/{x}/{y}.png',
+                    type: 'xyz'
+                }
+            }
+        },
+        defaults: {
+            scrollWheelZoom: false
+        }
+    });
 });
