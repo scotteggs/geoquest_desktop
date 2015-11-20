044fbdbfdf00a38da78a907dc2187a9b8f5a35c1
Scott <scotteggs@gmail.com>
2015-11-20 12:38:12 -0500

    updates to state

--- browser/js/map/map.js
+++ browser/js/map/map.js
@@ -1,45 +1,37 @@
 app.config(function ($stateProvider) {
 
     // Register our *about* state.
-    $stateProvider.state('about', {
-        url: '/about',
-        controller: 'AboutController',
-        templateUrl: 'js/about/about.html'
+    $stateProvider.state('map', {
+        url: '/map',
+        controller: 'MapController',
+        templateUrl: 'js/map/map.html'
     });
 
 });
 
-app.controller('AboutController', function ($scope, leafletData) {
+app.controller('MapController', function ($scope, leafletData) {
     // make map by addn some sh*t to the MF $scope!
     angular.extend($scope, {
         center: {
             autoDiscover: true
         },
         events: {},
-        defaults: {
-                    tileLayer: "https://api.tiles.mapbox.com/v4/scotteggs.o7614jl2/{z}/{x}/{y}.png",
-                    zoomControlPosition: 'topright',
-                    tileLayerOptions: {
-                        opacity: 0.9,
-                        detectRetina: true,
-                        reuseTiles: true,
-                    },
-                    scrollWheelZoom: false
-                },
-        defaults: {
-            scrollWheelZoom: false
-        }
     });
 
     $scope.getLocation = function() {
         leafletData.getMap('map').then(function(map) {
-            map.locate({setView: true, maxZoom: 16, watch: true, enableHighAccuracy: true});
+            console.log('map object', map);
+            map.locate({setView: true, maxZoom: 16, watch: false, enableHighAccuracy: true});
             map.on('locationfound', function (e) {
                 console.log(e.latlng, e.accuracy);
             });
         });
     };
     $scope.getLocation();
+
+
+
+
 
 
 });
