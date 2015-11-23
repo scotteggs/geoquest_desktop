a852653c5409992b1e56f6a3569f2c55a5f1700a
William Jacobson <williamjacobson@Williams-MacBook-Pro.local>
2015-11-23 09:36:34 -0500

    yes

--- browser/js/common/factories/RandomGreetings.js
+++ browser/js/common/factories/RandomGreetings.js
@@ -5,16 +5,7 @@ app.factory('RandomGreetings', function () {
     };
 
     var greetings = [
-        'Hello, world!',
-        'At long last, I live!',
-        'Hello, simple human.',
-        'What a beautiful day!',
-        'I\'m like any other project, except that I am yours. :)',
-        'This empty string is for Lindsay Levine.',
-        'こんにちは、ユーザー様。',
-        'Welcome. To. WEBSITE.',
-        ':D',
-        'Yes, I think we\'ve met before.'
+        'This is GeoQuest'
     ];
 
     return {
