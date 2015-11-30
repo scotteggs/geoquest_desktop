dff3c3831fca6a5d9f9bd92435adda5c2ecc6f5c
saferthanhouses <joey_oliver@hotmail.com>
2015-11-30 18:41:59 -0500

    editor template & resolve

--- browser/js/common/factories/games-factory.js
+++ browser/js/common/factories/games-factory.js
@@ -9,9 +9,19 @@ app.factory('GamesFactory', function($http) {
 			});
 		},
 
+		getOneGame: function(gameId){
+			// return $http.get('/api/games/' + gameId)
+			// 	.then(function(res){
+			// 		return res.data;
+			// 	})
+			var game = {states: []};
+			return game;
+		},
+
 		getUserGames: function(userId) {
 			var games = [
 				{
+					id: 0,
 		            name: 'Tour of Olde Shit',
 		            summary: 'Check out cool historical stuff in the area',
 		            time: '1.5',
@@ -19,6 +29,7 @@ app.factory('GamesFactory', function($http) {
 		            start: [40.713031, -74.009896]
 		        },
 		        {
+		        	id: 1,
 		            name: 'Kill the Thing',
 		            summary: 'Go to the place and stab the monsters.',
 		            time: '1',
@@ -26,6 +37,7 @@ app.factory('GamesFactory', function($http) {
 		            start: [66.664463, -150.225470]
 		        },
 		        {
+		        	id: 2,
 		            name: 'Save the Princess',
 		            summary: 'She\'s being forced into an unsavory marriage',
 		            time: '1.5',
@@ -33,6 +45,7 @@ app.factory('GamesFactory', function($http) {
 		            start: [37.752731, -122.450657]
 		        },
 		        {
+		        	id: 3,
 		            name: 'Destroy the ring',
 		            summary: '\"Cast it into the fountain!\"',
 		            time: '1',
@@ -40,6 +53,7 @@ app.factory('GamesFactory', function($http) {
 		            start: [40.840256, -73.924791]
 		        },
 		        {
+		        	id: 4,
 		            name: 'Ye Classic Drinking Quest',
 		            summary: 'Get proper pissed in the friendliest bars in town',
 		            time: '???',
@@ -47,6 +61,7 @@ app.factory('GamesFactory', function($http) {
 		            start: [40.745645, -73.978349]
 		        },
 		        {
+		        	id: 5,
 		            name: 'Escape the Trolls!',
 		            summary: 'If you seek some exercise',
 		            time: '1',
--- browser/js/quest-editor/editor.js
+++ browser/js/quest-editor/editor.js
@@ -6,10 +6,18 @@ app.config(function ($stateProvider){
 		controller: 'EditorCtrl',
 		data: {
             authenticate: true
+        },
+        resolve: {
+        	quest: function(GamesFactory, $stateParams){
+        		console.log("editor resole $stateParams", $stateParams)
+        		return $stateParams.gameId ? 
+        					GameFactory.getOneGame($stateParams.gameId) :
+        					undefined;
+        	}
         }
 	});
 });
 
-app.controller('EditorCtrl', function ($scope) {
-	$scope.name = "Editor";
+app.controller('EditorCtrl', function ($scope, quest) {
+	console.log("quest", quest);
 })
