2cf9a4391e4ab573789f1950b9555bf8b2c020a7
saferthanhouses <joey_oliver@hotmail.com>
2015-11-30 14:58:04 -0500

    adds editor & dashboard states

---
+++ browser/js/quest-editor/quest-editor.js
@@ -0,0 +1,15 @@
+app.config(function ($stateProvider){
+	$stateProvider.state('editor',
+	{
+		url: '/editor',
+		templateUrl: 'js/editor/editor.html',
+		controller: 'EditorCtrl',
+		data: {
+            authenticate: true
+        }
+	})
+})
+
+app.controller('EditorCtrl', function ($scope) {
+
+})
