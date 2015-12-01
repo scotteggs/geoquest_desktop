a5531e08ce8807d178452636aa8b9f6ef203938f
saferthanhouses <joey_oliver@hotmail.com>
2015-12-01 17:42:28 -0500

    mapSTATE

---
+++ server/db/models/mapstate (Case Conflict).js
@@ -0,0 +1,28 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new mongoose.Schema({
+    name: {
+        type: String,
+        max: 20
+    },
+    visibleRegions: {
+        ref: 'Region',
+        type: [Schema.Types.ObjectId],
+    },
+    modal: {
+        title: String,
+        imageUrl: String,
+        text: String
+    },
+    transitionCondition: {
+        name: String,
+        region: {
+            ref: 'Region',
+            type: Schema.Types.ObjectId
+        }
+    },
+});
+
+mongoose.model('MapState', schema);
