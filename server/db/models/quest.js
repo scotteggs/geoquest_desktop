ba8eee0a3e7c6211ad521a666ae3db0eec4dd796
Will Jacobson <willzjacobson@users.noreply.github.com>
2015-12-08 13:38:29 -0500

    logs out totalDistance virtua;

--- server/db/models/quest.js
+++ server/db/models/quest.js
@@ -72,8 +72,10 @@ schema.virtual('totalDistance').get(function(){
         var lon1 = this.questSteps[i-1].location[1];
         var lat2 = this.questSteps[i].location[0];
         var lon2 = this.questSteps[i].location[1];
+        console.log('one leg', getDistanceFromLatLonInMi(lat1, lon1, lat2, lon2));
         totalDistance += getDistanceFromLatLonInMi(lat1, lon1, lat2, lon2);
     }
+    consolelog('totalDistance', totalDistance)
     return totalDistance;
 });
 
