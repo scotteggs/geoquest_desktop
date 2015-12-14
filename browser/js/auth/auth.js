e025adce74b961c88053fb9eb8160f64709c6885
William Jacobson <williamjacobson@Williams-MacBook-Pro.local>
2015-12-14 14:03:17 -0500

    makes landing page gorgeous

--- browser/js/auth/auth.js
+++ browser/js/auth/auth.js
@@ -19,7 +19,7 @@ app.controller('AuthCtrl', function ($scope, AuthService, $state) {
         AuthService.login(loginInfo).then(function (user) {
             $state.go('dashboard', {userId: user._id});
         }).catch(function () {
-            $scope.error = 'Invalid login credentials.';
+            $scope.error = 'Either you\'ve entered the wrong info, or we already have someone by that name';
         });
 
     };
@@ -30,8 +30,28 @@ app.controller('AuthCtrl', function ($scope, AuthService, $state) {
             $state.go('dashboard', {userId: user._id}, {reload: true});
         }).catch(function(){
             $scope.error = "Invalid Signup Credentials";
-        })
+        });
+    };
 
-    }
+    // PARALAX EFFECT
+    $(function() {
+    // Cache the window object (makes load time faster)
+    var $window = $(window);
+    // Parallax background effect
+    $('section[data-type="background"]').each(function() {
+        var $bgobj = $(this); // assigning the object
+        $(window).scroll(function() {
+            //scroll the background at var speed
+            // the yPos is a negative because we're scrolling it up
+            var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+            // Put together our final background position
+            var coords = '50% ' + yPos + 'px';
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+        }); // end window scroll
+    });
+});
+
+
 
 });
--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -39,6 +39,27 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state) 
             $rootScope.$on(AUTH_EVENTS.logoutSuccess, removeUser);
             $rootScope.$on(AUTH_EVENTS.sessionTimeout, removeUser);
 
+            // Pretty Scrolling Navbar Effect
+            $(window).scroll(function() {
+                if ($('.navbar').offset().top > 50) {
+                    $('.navbar-fixed-top').addClass('top-nav-collapse');
+                } else {
+                    $('.navbar-fixed-top').removeClass('top-nav-collapse');
+                }
+            });
+
+            // Animated Scroll To Section
+            $(function() {
+                $('.page-scroll a').bind('click', function() {
+                    var $anchor = $(this);
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1500, 'easeInOutExpo');
+                    event.preventDefault();
+                });
+            });
+
+
         }
 
     };
