b887cc847fff0349df2d9b61db074406f759b9b5
William Jacobson <williamjacobson@Williams-MacBook-Pro.local>
2015-12-21 17:27:41 -0500

    amending loading screen

--- browser/js/common/directives/loading/loading.js
+++ browser/js/common/directives/loading/loading.js
@@ -5,8 +5,9 @@ app.directive('resolveLoader', function($rootScope, $timeout) {
     replace: true,
     template: '<div class="alert alert-success ng-hide"><strong>Welcome!</strong> Loading up! </div>',
     link: function(scope, element) {
-
+      console.log('loading dir')
       $rootScope.$on('$routeChangeStart', function(event, currentRoute, previousRoute) {
+        console.log('change start');
         if (previousRoute) return;
 
         $timeout(function() {
