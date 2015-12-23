afad380289761237a86af46680705765138c8ee2
William Jacobson <williamjacobson@Williams-MacBook-Pro.local>
2015-12-22 21:36:06 -0500

    added loading to main editor

--- browser/js/common/directives/loading/loading.js
+++ browser/js/common/directives/loading/loading.js
@@ -3,7 +3,7 @@ app.directive('resolveLoader', function($rootScope, $timeout) {
   return {
     restrict: 'E',
     replace: true,
-    template: '<div class="alert alert-success ng-hide"><strong>Loading up!</strong></div>',
+    template: '<div class="alert alert-success ng-hide"><strong>Loading up! Just a sec..</strong></div>',
     link: function(scope, element) {
       $rootScope.$on('$stateChangeStart', function(event, currentRoute, previousRoute) {
 
